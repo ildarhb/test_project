2b9884c2ee331fb2c67e0fbc70cde675b723fb40
ildarhb <ahmetgalin989@gmail.com>
2023-04-16 15:37:39 +0500

    #2 Добавили функцию для сложения

--- main.py
+++ main.py
@@ -1,16 +1,7 @@
-# This is a sample Python script.
+class Calculator:
+    def __init__(self, a: int, b: int):
+        self.a = a
+        self.b = b
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    def add(self):
+        return self.a + self.b
